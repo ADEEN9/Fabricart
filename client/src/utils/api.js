88a84a29647d5aad9f88b7a47d1ddc2d85b8090d
adeen9 <adeen2757@gmail.com>
2026-03-24 11:56:24 +0530

    server URl

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const SERVER_URL = 'http://localhost:5000';
+export const SERVER_URL = import.meta.env.VITE_SERVER_URL || 'http://localhost:5000';
 
 const API = axios.create({
     baseURL: `${SERVER_URL}/api`,
